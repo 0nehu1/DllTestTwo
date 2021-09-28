b829deb3ee22dd259d91b2d4a74dff7467d40e62
ONEHuI <onejun0320@gmail.com>
2021-09-28 17:27:33 +0900

    완성..!

--- DllTestTwo/CClientSocket.cpp
+++ DllTestTwo/CClientSocket.cpp
@@ -37,6 +37,25 @@ void AFX_EXT_CLASS CClientSocket::OnClose(int nErrorCode)
 void AFX_EXT_CLASS CClientSocket::OnReceive(int nErrorCode)
 {
 	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	
+	CString strTmp = _T(""), strIPAddress = _T("");
+	UINT uPortNumber = 0;
+	TCHAR szBuffer[1024];
+	::ZeroMemory(szBuffer, sizeof(szBuffer));
+
+	GetPeerName(strIPAddress, uPortNumber);
+	if (Receive(szBuffer, sizeof(szBuffer)) > 0)
+	{
+		CDLLChattingServerDlg* pMain = (CDLLChattingServerDlg*)AfxGetMainWnd();
+		strTmp.Format(_T("[%s:%d] : %s"), strIPAddress, uPortNumber, szBuffer);
+		pMain->m_List.AddString(strTmp);
+		pMain->m_List.SetCurSel(pMain->m_List.GetCount()-1);
+
+		CListenSocket* pServerSocket = (CListenSocket*)m_pListenSocket;
+		pServerSocket->SendChatDataAll(szBuffer);
+
+	}
+
 
 	CSocket::OnReceive(nErrorCode);
 }
