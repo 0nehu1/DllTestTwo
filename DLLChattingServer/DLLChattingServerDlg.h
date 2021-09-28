fd148cbb0e17b6c5806a7aa4a39cad91b13b457c
ONEHuI <onejun0320@gmail.com>
2021-09-28 15:54:46 +0900

    초기 커밋.

---
+++ DLLChattingServer/DLLChattingServerDlg.h
@@ -0,0 +1,39 @@
+﻿
+// DLLChattingServerDlg.h: 헤더 파일
+//
+#include "pch.h"
+#include "..\DllTestTwo\CListenSocket.h"
+#pragma once
+
+
+// CDLLChattingServerDlg 대화 상자
+class CDLLChattingServerDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CDLLChattingServerDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	CListenSocket m_ListenSocket;
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLLCHATTINGSERVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_List;
+	afx_msg void OnDestroy();
+
+};
---
+++ DllTestTwo/CClientSocket.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "CClientSocket.h"
+#include "..\DLLChattingServer\DLLChattingServerDlg.h"
+
+void CClientSocket::SetListenSocket(CAsyncSocket* pSocket)
+{
+	m_pListenSocket = pSocket;
+}
+
+
+
+void CClientSocket::OnClose(int nErrorCode)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	CString strTmp = _T(""), strIPAddress = _T("");
+	UINT uPortNumber = 0;
+	TCHAR szBuffer[1024];
+	::ZeroMemory(szBuffer, sizeof(szBuffer));
+
+	GetPeerName(strIPAddress, uPortNumber);
+	if (Receive(szBuffer, sizeof(szBuffer)) > 0)
+	{
+		CDLLChattingServerDlg* pMain = (CDLLChattingServerDlg*)AfxGetMainWnd();
+		strTmp.Format(_T("%s:%d] : %s"), strIPAddress, uPortNumber, szBuffer);
+		pMain->m_List.AddString(strTmp);
+		pMain->m_List.SetCurSel(pMain->m_List.GetCount() - 1);
+
+		CListenSocket* pServerSocket = (CListenSocket*)m_pListenSocket;
+		pServerSocket->SendChatDataAll(szBuffer);
+	}
+
+
+	CSocket::OnClose(nErrorCode);
+}
+
+
+void CClientSocket::OnReceive(int nErrorCode)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+
+	CSocket::OnReceive(nErrorCode);
+}
